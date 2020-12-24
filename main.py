b29ec2c0709dc151e7695890d325bdc69c09cb89
rahman.taufik <rahman.taufik@loratechai.com>
2020-12-24 14:02:02 +0700

    rapid api

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import time
 
 from web_scraping import ingest_wsj, ingest_wsj_archive, ingest_barrons, ingest_benzinga, ingest_google_news, ingest_wiki
+from rapid_api import ingest_yahoo_finance, ingest_seeking_alpha, ingest_seeking_alpha_article, ingest_morningstar
 from user_agent import list_test
 
 parser = argparse.ArgumentParser()
@@ -15,6 +16,10 @@ parser.add_argument('--ingest_wsj', action='store_true', help='ingest wall stree
 parser.add_argument('--ingest_wsj_archive', action='store_true', help='ingest archive wall street journal')
 parser.add_argument('--ingest_barrons', action='store_true', help='ingest barrons news')
 parser.add_argument('--ingest_benzinga', action='store_true', help='ingest benzinga news')
+parser.add_argument('--ingest_yahoo_finance', action='store_true', help='ingest yahoo finance news')
+parser.add_argument('--ingest_seeking_alpha', action='store_true', help='ingest seeking alpha news')
+parser.add_argument('--ingest_seeking_alpha_article', action='store_true', help='ingest seeking alpha article')
+parser.add_argument('--ingest_morningstar', action='store_true', help='ingest morningstar news')
 parser.add_argument('--headers_list_test', action='store_true', help='test list of headers used for google scrap')
 args = parser.parse_args()
 
@@ -31,6 +36,15 @@ if __name__ == '__main__':
         ingest_barrons()
     elif args.ingest_benzinga:
         ingest_benzinga()
+
+    if args.ingest_yahoo_finance:
+        ingest_yahoo_finance()
+    elif args.ingest_seeking_alpha:
+        ingest_seeking_alpha()
+    elif args.ingest_seeking_alpha_article:
+        ingest_seeking_alpha_article()
+    elif args.ingest_morningstar:
+        ingest_morningstar()
     
     if args.headers_list_test:
         list_test()
--- rapid_api.py
+++ rapid_api.py
@@ -36,15 +36,16 @@ def ingest_yahoo_finance():
     # convert into json
     res = json.loads(response.text)
 
-    length = len(res)
-    print("total data news: ", length)
+    # length = len(res)
+    # print("total data news: ", length)
 
-    # store to csv
-    open_file = open('data_corpus/news/yahoo_finance.json', 'w')
+    # store to json file
+    open_file = open('data_news/yahoo_finance.json', 'w')
     open_file.write(json.dumps(res, indent=4, sort_keys=True))
     open_file.close()
 
 def ingest_seeking_alpha():
+    # get list of news first
     url = "https://seeking-alpha.p.rapidapi.com/news/list"
 
     querystring = {"id":"aapl","size":"100","until":"0"}
@@ -57,10 +58,11 @@ def ingest_seeking_alpha():
     response = requests.request("GET", url, headers=headers, params=querystring)
     res = json.loads(response.text)
   
-    open_file = open('data_corpus/news/seeking_alpha_news.json', 'w')
+    open_file = open('data_news/seeking_alpha_news.json', 'w')
     open_file.write(json.dumps(res, indent=4, sort_keys=True))
     open_file.close()
 
+    # then get detail news from id of news list
     url2 = "https://seeking-alpha.p.rapidapi.com/news/get-details"
 
     # looping id
@@ -68,7 +70,55 @@ def ingest_seeking_alpha():
     res2_all = []
 
     for list in list_data:
-        print(list['id'])
+        # set id into query
+        querystring2 = {"id":list['id']}
+
+        headers2 = {
+            'x-rapidapi-key': os.getenv("rapidapi_key"),
+            'x-rapidapi-host': "seeking-alpha.p.rapidapi.com"
+            }
+
+        try:
+            response2 = requests.request("GET", url2, headers=headers2, params=querystring2)
+            res2 = json.loads(response2.text)
+            res2_all.append(res2)
+
+        # if there is no data skip and continue the looping
+        except:
+            continue
+
+    length2 = len(res2_all)
+    print("total data for news: ", length2)
+  
+    open_file2 = open('data_news/seeking_alpha_news_detail.json', 'w')
+    open_file2.write(json.dumps(res2_all, indent=4, sort_keys=True))
+    open_file2.close()
+
+def ingest_seeking_alpha_article():
+    # similar with seeking alpha news
+    url = "https://seeking-alpha.p.rapidapi.com/articles/list"
+
+    querystring = {"filterCategory":"latest-articles","until":"0","size":"100"}
+
+    headers = {
+        'x-rapidapi-key': os.getenv("rapidapi_key"),
+        'x-rapidapi-host': "seeking-alpha.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    res = json.loads(response.text)
+    
+    open_file = open('data_news/seeking_alpha_article.json', 'w')
+    open_file.write(json.dumps(res, indent=4, sort_keys=True))
+    open_file.close()
+
+    url2 = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
+
+    # looping id
+    list_data = res['data']
+    res2_all = []
+
+    for list in list_data:
         querystring2 = {"id":list['id']}
 
         headers2 = {
@@ -84,56 +134,38 @@ def ingest_seeking_alpha():
             continue
 
     length2 = len(res2_all)
-    print("total data for news: ", length2)
-  
-    open_file2 = open('data_corpus/news/seeking_alpha_news_detail.json', 'w')
+    print("total data for articles detail: ", length2)
+    
+    open_file2 = open('data_news/seeking_alpha_article_detail.json', 'w')
     open_file2.write(json.dumps(res2_all, indent=4, sort_keys=True))
     open_file2.close()
 
-def ingest_seeking_alpha_article():
-    url = "https://seeking-alpha.p.rapidapi.com/articles/list"
-
-    querystring = {"filterCategory":"latest-articles","until":"0","size":"100"}
-
-    headers = {
-        'x-rapidapi-key': os.getenv("rapidapi_key"),
-        'x-rapidapi-host': "seeking-alpha.p.rapidapi.com"
-        }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    res = json.loads(response.text)
-
-    length = len(res)
-    print("total data for articles: ", length)
-    
-    open_file = open('data_corpus/news/seeking_alpha_article.json', 'w')
-    open_file.write(json.dumps(res, indent=4, sort_keys=True))
-    open_file.close()
-
-    url2 = "https://seeking-alpha.p.rapidapi.com/articles/get-details"
-
-    # example id
-    querystring2 = {"id":"4395547"}
-
-    headers2 = {
-        'x-rapidapi-key': os.getenv("rapidapi_key"),
-        'x-rapidapi-host': "seeking-alpha.p.rapidapi.com"
-        }
-
-    response2 = requests.request("GET", url2, headers=headers2, params=querystring2)
-    res2 = json.loads(response2.text)
-
-    length2 = len(res2)
-    print("total data for articles detail: ", length2)
-    
-    open_file2 = open('data_corpus/news/seeking_alpha_article_detail.json', 'w')
-    open_file2.write(json.dumps(res2, indent=4, sort_keys=True))
-    open_file2.close()
+# def join_duplicate_keys(ordered_pairs):
+#     d = {}
+#     for k, v in ordered_pairs:
+#         if k in d:
+#             if type(d[k]) == list:
+#                 d[k].append(v)
+#             else:
+#                 newlist = []
+#                 newlist.append(d[k])
+#                 newlist.append(v)
+#                 d[k] = newlist
+#         else:
+#             d[k] = v
+#     return d
 
 def ingest_morningstar():
 
-    def parse_object_pairs(pairs):
-        return pairs
+    # join duplicate keys (it's not fixed yet)
+    def join_duplicate_keys(pairs):
+        d = {}
+        for k, v in pairs:
+            if k not in d:
+                d[k] = v
+            else:
+                d[k] += v
+        return d
 
     url = "https://morning-star.p.rapidapi.com/news/list"
 
@@ -146,16 +178,12 @@ def ingest_morningstar():
 
     response = requests.request("GET", url, headers=headers, params=querystring)
     res = json.loads(response.text)
-
-    length = len(res)
-    print("total data: ", length)
   
-    open_file = open('data_corpus/news/morningstar_news.json', 'w')
+    open_file = open('data_news/morningstar_news.json', 'w')
     open_file.write(json.dumps(res, indent=4, sort_keys=True))
     open_file.close()
 
     url2 = "https://morning-star.p.rapidapi.com/news/get-details"
-
 
     res2_all = []
     for r in res:
@@ -169,8 +197,10 @@ def ingest_morningstar():
 
         try:
             response2 = requests.request("GET", url2, headers=headers2, params=querystring2)
-            decoder = JSONDecoder(object_pairs_hook=parse_object_pairs)
-            res2 = decoder.decode(response2)
+            # decoder = JSONDecoder(object_pairs_hook=parse_object_pairs)
+            # res2 = decoder.decode(response2)
+            # res2_all.append(res2)
+            res2 = json.loads(response2.text, object_pairs_hook=join_duplicate_keys)
             res2_all.append(res2)
         except:
             continue
@@ -178,6 +208,6 @@ def ingest_morningstar():
     length2 = len(res2_all)
     print("total data detail news: ", length2)
   
-    open_file2 = open('data_corpus/news/morningstar_news_detail.json', 'w')
+    open_file2 = open('data_news/morningstar_news_detail.json', 'w')
     open_file2.write(json.dumps(res2_all, indent=4, sort_keys=True))
     open_file2.close()
